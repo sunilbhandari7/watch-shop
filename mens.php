06ff778d0c621b28b4d309a89234b0ab6ff02338
sunilbhandari7 <sunilbhandari2021@gmail.com>
2024-05-12 08:12:46 +0545

    css added

--- mens.php
+++ mens.php
@@ -10,7 +10,7 @@
     
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     
     
     <link rel="stylesheet" href="mens.css">
